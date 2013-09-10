daf02b10e98b103b9dc727085dcddadf0602b370
cooltange <cooltange@qq.com>
2013-09-10 23:50:12 +0800

    modified

--- stx52-demo/src/com/stx/s1/p199/P202.java
+++ stx52-demo/src/com/stx/s1/p199/P202.java
@@ -9,12 +9,12 @@ package com.stx.s1.p199;
 public class P202 {
 
 	public static void main(String[] args) {
-		// 向上变换
+		// 向上变换(自动转换)
 		Duck duck = new DonaDuck();
 
 		// 检测duck对象是否为唐老鸭类的对象
 		if (duck instanceof DonaDuck) {
-			// 向下变换
+			// 向下变换(强制转换)
 			DonaDuck dona = (DonaDuck) duck;
 
 			dona.greet();
